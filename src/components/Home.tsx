9420f13b1f36fb3e732476469ae3d2aaf7fb0d3e
Evgeni Kiradzhiyski <evkirad@gmail.com>
2021-09-27 01:03:03 +0200

    :wrench: Adds user service

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import UserService from '../services/user.service';
 import Page from './Page';
 
-const Home: React.FC<{}> = () => (<Page>{/* TODO: Main conent */}</Page>);
+const Home: React.FC<{}> = () => {
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            console.log(`Calling user`)
+            await UserService.fetchUsers();
+        }
+
+        fetchUsers();
+    }, [])
+
+    return <Page>{/* TODO: Main conent */}</Page>
+}
 
 export default Home;
 
---
+++ src/services/user.service.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosResponse } from "axios"
+
+const BASE_URI = `api/v1`
+const COLLECTION = `users`
+
+const UserService = {
+    fetchUsers: async(limit: number = 2000): Promise<AxiosResponse> => (await axios.get(`/${BASE_URI}/${COLLECTION}`)).data.data
+}
+
+export default UserService
