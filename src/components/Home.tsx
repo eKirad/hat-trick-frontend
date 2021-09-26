676f53523826b82278b0fe4646cb47c71834dfe4
Evgeni Kiradzhiyski <evkirad@gmail.com>
2021-09-27 00:04:43 +0200

    :recycle: Refactors components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
-
-// Component imports
 import Page from './Page';
 
-interface IProps {
-
-}
-
-const Home: React.FC<IProps> = (props) => {
-
-    return(
-        <Page>
-            <p>Main content</p>
-        </Page>
-    );
-}
+const Home: React.FC<{}> = () => (<Page><p>Main content</p></Page>);
 
 export default Home;
 
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,23 +1,14 @@
-// React imports
+
 import React, { useState } from 'react';
-
-
-// Component imports
 import Header from './Header';
 import Footer from './Footer';
-
-// Service imports
 import UserService from '../services/UserService';
 
-interface IProps {
-
-};
-
-const Page: React.FC<IProps> = (props) => {
+const Page: React.FC<{}> = (props) => {
     const currentUser = UserService.isAuthenticated() ? UserService.getCurrentUser() : { };
     const [user, setUser] = useState(currentUser);
 
-    return(
+    return (
         <section>
             <Header
                 user ={user}    
