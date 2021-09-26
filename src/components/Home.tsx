2a36b8d51b7653733498dcc7621378f5d20117b0
Evgeni Kiradzhiyski <evkirad@gmail.com>
2021-09-27 01:21:56 +0200

    :art: Adds user card to home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import UserService from '../services/user.service';
 import Page from './Page';
+import UserCard from './UserCard';
 
 const Home: React.FC<{}> = () => {
+    const [users, setUsers] = useState<any>();
+
 
     useEffect(() => {
         const fetchUsers = async () => {
             console.log(`Calling user`)
-            await UserService.fetchUsers();
+            const users = await UserService.fetchUsers();
+            setUsers(users);
         }
 
         fetchUsers();
     }, [])
 
-    return <Page>{/* TODO: Main conent */}</Page>
+    return (
+        <Page>
+            { users && users.map((user: any) => (<UserCard user={user}/>))}
+        </Page>
+    )
 }
 
 export default Home;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,7 +4,7 @@ const BASE_URI = `api/v1`
 const COLLECTION = `users`
 
 const UserService = {
-    fetchUsers: async(limit: number = 2000): Promise<AxiosResponse> => (await axios.get(`/${BASE_URI}/${COLLECTION}`)).data.data
+    fetchUsers: async(limit: number = 2000): Promise<AxiosResponse<any>> => (await axios.get(`/${BASE_URI}/${COLLECTION}`)).data.data
 }
 
 export default UserService
